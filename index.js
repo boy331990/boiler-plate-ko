7e558ae677fbb4876ed30ac2c1c6b09bdea41a4e
ukss <boy331990@naver.com>
2020-04-16 12:04:28 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
 var port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const {User} = require("./models/User");
+
+
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ukss:abcd1234@youtubeclone-ajxbf.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -11,7 +24,22 @@ mongoose.connect('mongodb+srv://ukss:abcd1234@youtubeclone-ajxbf.mongodb.net/tes
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world 할룽~');
+  res.send('hello world 할룽~ 얄리얄리얄라셩123123123123');
 });
+
+app.post('/register', (req, res) => {
+  
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
